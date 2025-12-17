47bd151c07ed53373bc42debaeeb9bbd743317c0
=amitpathlock <=amit.kumar@pathlock.com>
2025-12-17 11:12:25 +0530

    new changes

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,16 @@
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+	js.configs.recommended,
 	{
-		ignores: ["dist/","webapp/test/","node_modules/"],
+		// This configuration applies to all files except those in 'myFolder'
+		files: ["**/*.js"], // applies to all JS files first
+		ignores: [
+			// For non-global ignores, a full glob pattern is required to match files in subdirectories
+			'dist/**',
+			'webapp/test/**',
+			'node_modules/**'
+		],
 		rules: {
 			semi: "error",
 			"prefer-const": "error",
