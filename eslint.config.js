52a553ac05f808862d0cb8ea82d811074edc775f
=amitpathlock <=amit.kumar@pathlock.com>
2025-12-17 10:07:49 +0530

    added new config file

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+import eslint from '@eslint/js';
+
+export default [
+  {
+    // The "ignores" property takes an array of glob patterns
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "webapp/test/"
+    ],
+  },
+  eslint.configs.recommended,
+  // other configurations...
+];
