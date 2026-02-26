e37f1f8a7c7e7cee81fd52868661a7f184884d2c
=amitpathlock <=amit.kumar@pathlock.com>
2026-02-26 11:04:18 +0530

    Git Action

--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -1,10 +1,10 @@
 sap.ui.define([
-	// "pl/dac/apps/fnconfig/test/unit/controller/Master.controller",
-	// "pl/dac/apps/fnconfig/test/unit/controller/DataMaskingEnforcement.controller",
-	// "pl/dac/apps/fnconfig/test/unit/controller/DataRestrictionEnforcement.controller",
-	// "pl/dac/apps/fnconfig/test/unit/controller/DataAttributes.controller",
-	// "pl/dac/apps/fnconfig/test/unit/controller/UserAttributes.controller",
-	//"pl/dac/apps/fnconfig/test/unit/controller/Policies.controller",
+	"pl/dac/apps/fnconfig/test/unit/controller/Master.controller",
+	"pl/dac/apps/fnconfig/test/unit/controller/DataMaskingEnforcement.controller",
+	"pl/dac/apps/fnconfig/test/unit/controller/DataRestrictionEnforcement.controller",
+	"pl/dac/apps/fnconfig/test/unit/controller/DataAttributes.controller",
+	"pl/dac/apps/fnconfig/test/unit/controller/UserAttributes.controller",
+	"pl/dac/apps/fnconfig/test/unit/controller/Policies.controller",
 	"pl/dac/apps/fnconfig/test/unit/controller/RuleBuilder.controller"
 ], function () {
 	"use strict";
